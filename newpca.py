617201ffa6f5ee24601d9803b50cf82790d00aba
yliwang <1220264221@qq.com>
2022-11-23 15:13:48 +0800

    Add files via upload

---
+++ newpca.py
@@ -0,0 +1,17 @@
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def pca( x, components=-1 ):
+    xhat = None
+
+    return xhat
+
+
+if __name__=='__main__':
+    print("")
+
+
+
+
